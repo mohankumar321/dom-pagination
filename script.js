2081ba4e691e939942d0abf7b22e35b80e54ff03
mohankumar321 <smkmohan321@gmail.com>
2023-01-01 21:58:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let page=document.createElement('div');
+document.body.append(page)
+
+page.innerHTML=`<div class="pagination">
+<a  href="#">First</a>
+<a href="#">Previous</a>
+<a  href="#">&laquo;</a>
+<a href="#">1</a>
+<a class="active" href="#">2</a>
+<a  href="#">3</a>
+<a  href="#">4</a>
+<a  href="#">5</a>
+<a  href="#" >6</a>
+<a  href="#">7</a>
+<a   href="#">8</a>
+<a  href="#">9</a>
+<a  href="#">10</a>
+<a href="#">&raquo;</a>
+</div> `
+console.log(document)
